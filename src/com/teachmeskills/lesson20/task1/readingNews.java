d1995efe0a5de8b08516bec84d3602423793291f
Alina-Ilkevich <alina.1qaz@gmail.com>
2024-03-17 16:52:26 +0300

    1

---
+++ src/com/teachmeskills/lesson20/task1/readingNews.java
@@ -0,0 +1,10 @@
+package com.teachmeskills.lesson20.task1;
+
+public class readingNews extends Thread{
+    private String threadName;
+
+    public readingNews(String name) {
+        super(name);
+        this.threadName = threadName;
+    }
+}
